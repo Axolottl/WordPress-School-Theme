330d3317ebb4a5cc932cc87c003cd6905ac46210
elfaroki-marwane <elfaroki.m.i@gmail.com>
2020-03-29 18:35:25 +0200

    footer design commit

--- school theme/footer.php
+++ school theme/footer.php
@@ -1,7 +1,21 @@
-	<div class="footer text-center ">
-		Copyright: &copy; 2020 EST-SB
-	</div>
+<hr class="my-4">
+	
+	<div class="card text-center ">
+  <div class="card-header">
+    
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Thanks fo visiting</h5>
+    <p class="card-text">L’école Supérieure De Technologie De Sidi-Bennour</p>
+    <a href="#" class="btn btn-primary">Go To the Top</a>
+  </div>
+  <div class="card-footer text-muted">
+    &copy; Website designed for educational purposes by students from <?php bloginfo('name');?> <?php the_time('Y');?> 
+  </div>
+</div>
+ 
+  
 
-	<?php wp_footer();?>
+<?php wp_footer();?>
 </body>
 </html>
