e05e8a38f659909b75e6b09b4b4c25c774707739
elfaroki-marwane <elfaroki.m.i@gmail.com>
2020-03-29 18:23:11 +0200

    header commit

--- school theme/header.php
+++ school theme/header.php
@@ -2,13 +2,16 @@
 <html <?php language_attributes();?>>
 	<head>
 		<meta charset="<?php bloginfo('charset');?>">
-		<title><?php bloginfo('name');?></title>
+		<title>
+			<?php wp_title('|','true','right');?>
+			<?php bloginfo('name');?>
+		</title>
 		<?php wp_head();?>
 	</head>
-<body <?php body_class();?>>
+<body <?php body_class();?> data-spy="scroll" data-target="#menu" style="background-image:url(<?php bloginfo('stylesheet_directory');?>/images/landings.png) !important">
 	<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
 		<div class="container-fluid">
-		  <a class="navbar-brand" href="#"><img width="30px" height="30px" src="img/estsb.png"> <?php bloginfo('name');?></a>
+		  <a class="navbar-brand" href="#"><img width="30px" height="30px" src="<?php bloginfo('stylesheet_directory');?>/images/images.png"> <?php bloginfo('name');?></a>
 		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
 		<span class="navbar-toggler-icon" ></span>
  		 </button>
@@ -21,4 +24,5 @@
 		</div>
 		
 </nav>
+
    
