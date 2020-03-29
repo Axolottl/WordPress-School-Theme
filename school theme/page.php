c20577032a030917d154dee91fc42144d2ba50a5
elfaroki-marwane <elfaroki.m.i@gmail.com>
2020-03-29 18:33:44 +0200

    page design commit

--- school theme/page.php
+++ school theme/page.php
@@ -24,12 +24,24 @@
 	<div class="jumbotron jumbotron-fluid">
 	  <div class="container text-center">
 	    <h1 class="display-4"><?php the_title();?></h1>
-	    <?php if(have_posts()) : while (have_posts()): the_post() ;?>
-					<?php the_content();?>
-			<?php endwhile; endif;?>
 	  </div>
 	</div>
 	<hr class="my-4">
-	
-
+	<div class="container">
+        <div class="row">
+        	
+        	 <div class="page-content col-sm-6">
+	  <?php if(have_posts()) : while (have_posts()): the_post() ;?>
+					<?php the_content();?>
+			<?php endwhile; endif;?> 
+			</div>
+			<div class="page-image col-sm-6">
+				<?php if (has_post_thumbnail($post->ID)): ?>
+            <?php $image = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'single-post-thumbnail'); ?>
+            <img class="img-fluid  img-thumbnail" src="<?php echo $image[0]; ?>" alt=""  >
+        <?php endif; ?>
+			</div>
+		
+		</div>
+	</div>
 <?php get_footer();?>
