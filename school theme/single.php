028c6775ffeae6eaa78a11bdf0e9dd96e8e16ed3
elfaroki-marwane <elfaroki.m.i@gmail.com>
2020-03-29 18:25:03 +0200

    single  commit

--- school theme/single.php
+++ school theme/single.php
@@ -1,1 +1,21 @@
- 
+<?php get_header();?>
+
+
+
+ <div class="jumbotron jumbotron-fluid">
+      <div class="container text-center">
+        <h1 class="display-4"><?php the_title();?></h1>
+      </div>
+      <div class="container text-center">
+         <?php if(have_posts()) : while (have_posts()): the_post() ;?>
+                    <?php the_content();?>
+            <?php endwhile; endif;?> 
+        </div>
+</div>
+
+
+
+
+
+
+<?php get_footer();?>  
